1814e419021bea31ebeb98ad138631e2478736ab
Nguyenducanhtuanictusth <101237088+Nguyenducanhtuanictusth@users.noreply.github.com>
2022-03-16 00:07:02 +0700

    Add files via upload

---
+++ Student.py
@@ -0,0 +1,25 @@
+class Student:
+ 
+    def num_student():
+        num = input("Number student is: ")
+        return num
+        
+    def infor_student():
+        Student = ("" , "" , "")
+        print("enter information student: \n")    
+        Student[0] = input("ID: ")
+        Student[1] = input("Name: ")
+        Student[2] = input("Dob")
+        return Student
+
+    def number_course():
+        num = input("number of course is: ")
+
+
+    def Id_stu():
+        id = input("id student is:")
+
+
+    def name_course():
+        name_course = input("name of course is:")
+    
